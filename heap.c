336c03c5c3f9843485e87d9ca263ec40303df953
larcanee <larcane@uw.edu>
2023-11-06 21:07:19 -0800

    comments added - need DOUBLE CHECK

--- heap.c
+++ heap.c
@@ -43,7 +43,7 @@ int m2a(int sram_addr) {
  * @return the corresponding Cortex-M's SRAM address in an integer.
  */
 int a2m(int array_index) {
-  return array_index + 0x20000000; // converts an array index into a memory address and returns in
+  return array_index + 0x20000000; // converts an array index into a memory address and returns it
 }
 
 /*
@@ -51,15 +51,15 @@ int a2m(int array_index) {
  * to MCB: 0x2006800 - 0x20006C00.
  */
 void printArray() {
-  printf("memory ............................\n");
+  printf("memory ............................\n"); 
   // DOUBLE CHECK
-  for (int i = 0; i < 0x8000; i += 2) { // iterates through the MCB
-    if (a2m(i) >= 0x20006800) { // if the current array index is inside the MCB
-      printf("%x = %x(%d)\n", a2m(i), *(short *)&array[i], *(short *)&array[i]); // prints SRAM address, 16-bit int stored at that address, and the same value as a decimal
+  for (int i = 0; i < 0x8000; i += 2) { // iterates through the MCB (one entry is two bytes)
+    if (a2m(i) >= 0x20006800) { // if the current memory address is inside the MCB
+      printf("%x = %x(%d)\n", a2m(i), *(short *)&array[i], *(short *)&array[i]); // prints address, value, and the same value as a decimal of current memory address
     }
-    if (a2m(i + 2) >= 0x20006800) { // if the next array index is inside the MCB
-      printf("%x = %x(%d)\n", a2m(i + 2), *(short *)&array[i + 2],
-             *(short *)&array[i + 2]); // prints SRAM address, 16-bit int stored at that address, and the same value as a decimal
+    if (a2m(i + 1) >= 0x20006800) { // if the next memory address is inside the MCB
+      printf("%x = %x(%d)\n", a2m(i + 1), *(short *)&array[i + 1],
+             *(short *)&array[i + 1]); // prints address, value, and the same value as a decimal of next memory address
     }
   }
 }
@@ -115,36 +115,36 @@ void printMemory() {
  */
 void *_ralloc(int size, int left_mcb_addr, int right_mcb_addr) {
   // initial parameter computation
-  //  TODO(student): add comment to each of the following line of code
-  int entire_mcb_addr_space = right_mcb_addr - left_mcb_addr + mcb_ent_sz;
-  int half_mcb_addr_space = entire_mcb_addr_space / 2;
-  int midpoint_mcb_addr = left_mcb_addr + half_mcb_addr_space;
-  int heap_addr = 0;
-  int act_entire_heap_size = entire_mcb_addr_space * 16;
-  int act_half_heap_size = half_mcb_addr_space * 16;
+  //  DOUBLE CHECK
+  int entire_mcb_addr_space = right_mcb_addr - left_mcb_addr + mcb_ent_sz; //calculates current MCB size
+  int half_mcb_addr_space = entire_mcb_addr_space / 2; // divides MCB in half for buddy allocation
+  int midpoint_mcb_addr = left_mcb_addr + half_mcb_addr_space; // calculates the midpoint for the current MCB addr space (in hex)
+  int heap_addr = 0; // initialize heap address to zero
+  int act_entire_heap_size = entire_mcb_addr_space * 16; // converts entire heap size into hex (actual size)
+  int act_half_heap_size = half_mcb_addr_space * 16; // converts half heap size into hex (actual size0)
 
   // base case
-  //  TODO(student): add comment to each of the following line of code
-  if (size <= act_half_heap_size) {
+  //  DOUBLE CHECK
+  if (size <= act_half_heap_size) { // if the size is in the first half of the MCB
     void *heap_addr =
-        _ralloc(size, left_mcb_addr, midpoint_mcb_addr - mcb_ent_sz);
-    if (heap_addr == 0) {
-      return _ralloc(size, midpoint_mcb_addr, right_mcb_addr);
+        _ralloc(size, left_mcb_addr, midpoint_mcb_addr - mcb_ent_sz); // try to allocate memory in first half of the heap
+    if (heap_addr == 0) { // if allocation fails
+      return _ralloc(size, midpoint_mcb_addr, right_mcb_addr); // try to allocate memory in the second half of the heap
     }
-    if ((array[m2a(midpoint_mcb_addr)] & 0x01) == 0) {
-      *(short *)&array[m2a(midpoint_mcb_addr)] = act_half_heap_size;
+    if ((array[m2a(midpoint_mcb_addr)] & 0x01) == 0) { // if midpoint is unallocated memory
+      *(short *)&array[m2a(midpoint_mcb_addr)] = act_half_heap_size; // update MCB with actual size
     }
-    return heap_addr;
+    return heap_addr; // return allocated address
   }
   // (size > act_half_heap_size)
-  if ((array[m2a(left_mcb_addr)] & 0x01) != 0) {
-    return 0;
+  if ((array[m2a(left_mcb_addr)] & 0x01) != 0) { // if left address is in use
+    return 0; // indicates left address is in use
   }
-  if (*(short *)&array[m2a(left_mcb_addr)] < act_entire_heap_size) {
-    return 0;
+  if (*(short *)&array[m2a(left_mcb_addr)] < act_entire_heap_size) { // if left side of the heap has less memory than requested
+    return 0; // MCB in left address has less memory than requested
   }
-  *(short *)&array[m2a(left_mcb_addr)] = act_entire_heap_size | 0x01;
-  return (void *)(heap_top + (left_mcb_addr - mcb_top) * 16);
+  *(short *)&array[m2a(left_mcb_addr)] = act_entire_heap_size | 0x01; // indicate that left address is in use
+  return (void *)(heap_top + (left_mcb_addr - mcb_top) * 16); // return pointer to allocated memory
 }
 
 /*
@@ -156,46 +156,46 @@ void *_ralloc(int size, int left_mcb_addr, int right_mcb_addr) {
  * @return the same as the mcb_addr argument in success, otherwise 0.
  */
 int _rfree(int mcb_addr) {
-  //  TODO(student): add comment to each of the following line of code
-  short mcb_contents = *(short *)&array[m2a(mcb_addr)];
-  int mcb_index = mcb_addr - mcb_top;
+  //  DOUBLE CHECK
+  short mcb_contents = *(short *)&array[m2a(mcb_addr)]; // get contents of MCB entry at specified address
+  int mcb_index = mcb_addr - mcb_top; // calculate the index of MCB entry
   short mcb_disp = (mcb_contents /= 16);
   short my_size = (mcb_contents *= 16);
 
   // mcb_addr's used bit was cleared
   *(short *)&array[m2a(mcb_addr)] = mcb_contents;
 
-  //  TODO(student): add comment to each of the following line of code
-  if ((mcb_index / mcb_disp) % 2 == 0) {
-    if (mcb_addr + mcb_disp >= mcb_bot) {
+  //  DOUBLE CHECK
+  if ((mcb_index / mcb_disp) % 2 == 0) { // if MCB index is even (right child)
+    if (mcb_addr + mcb_disp >= mcb_bot) { // if the buddy is beyond MCB bottom boundary
       return 0; // my buddy is beyond mcb_bot!
     }
-    short mcb_buddy = *(short *)&array[m2a(mcb_addr + mcb_disp)];
-    if ((mcb_buddy & 0x0001) == 0) {
-      mcb_buddy = (mcb_buddy / 32) * 32;
-      if (mcb_buddy == my_size) {
-        *(short *)&array[m2a(mcb_addr + mcb_disp)] = 0;
-        my_size *= 2;
-        *(short *)&array[m2a(mcb_addr)] = my_size;
-        return _rfree(mcb_addr);
+    short mcb_buddy = *(short *)&array[m2a(mcb_addr + mcb_disp)]; // get MCB entry's buddy's content
+    if ((mcb_buddy & 0x0001) == 0) { // if buddy's memory is available
+      mcb_buddy = (mcb_buddy / 32) * 32; // calculate buddy size
+      if (mcb_buddy == my_size) { // if buddy can merge with MCB entry
+        *(short *)&array[m2a(mcb_addr + mcb_disp)] = 0; // clear buddy's used bit
+        my_size *= 2; // add buddy's size to MCB entry's size
+        *(short *)&array[m2a(mcb_addr)] = my_size; // update MCB entry's size
+        return _rfree(mcb_addr); // deallocate merged space
       }
     }
-  } else {
-    if (mcb_addr - mcb_disp < mcb_top) {
+  } else { // if MCB index is odd (left child)
+    if (mcb_addr - mcb_disp < mcb_top) { // if buddy is beyond MCB top boundary
       return 0; // my buddy is below mcb_top!
     }
-    short mcb_buddy = *(short *)&array[m2a(mcb_addr - mcb_disp)];
-    if ((mcb_buddy & 0x0001) == 0) {
-      mcb_buddy = (mcb_buddy / 32) * 32;
-      if (mcb_buddy == my_size) {
-        *(short *)&array[m2a(mcb_addr)] = 0;
-        my_size *= 2;
-        *(short *)&array[m2a(mcb_addr - mcb_disp)] = my_size;
-        return _rfree(mcb_addr - mcb_disp);
+    short mcb_buddy = *(short *)&array[m2a(mcb_addr - mcb_disp)]; // get MCB entry's buddy's content 
+    if ((mcb_buddy & 0x0001) == 0) { // if buddy's memory is available
+      mcb_buddy = (mcb_buddy / 32) * 32; // calculate buddy size
+      if (mcb_buddy == my_size) { // if buddy can merge with MCB entry
+        *(short *)&array[m2a(mcb_addr)] = 0; // clear buddy's used bit
+        my_size *= 2; // add buddy's size to MCB entry's size
+        *(short *)&array[m2a(mcb_addr - mcb_disp)] = my_size; // update MCB entry's size
+        return _rfree(mcb_addr - mcb_disp); // deallocate merged space
       }
     }
   }
-  return mcb_addr;
+  return mcb_addr; // return original address if merges or buddy checks fail
 }
 
 /*
@@ -204,15 +204,15 @@ int _rfree(int mcb_addr) {
  * driver.c's main( ).
  */
 void _kinit() {
-  //  TODO(student): add comment to each of the following line of code
-  for (int i = 0x20001000; i < 0x20005000; i++) {
-    array[m2a(i)] = 0;
+  //  DOUBLE CHECK
+  for (int i = 0x20001000; i < 0x20005000; i++) { // traverse heap space from 0x20001000 to 0x20004FFF
+    array[m2a(i)] = 0; // initialize each memory address to 0 to indicate unallocated memory
   }
 
-  *(short *)&array[m2a(mcb_top)] = max_size;
-  for (int i = 0x20006802; i < 0x20006C00; i += 2) {
-    array[m2a(i)] = 0;
-    array[m2a(i + 1)] = 0;
+  *(short *)&array[m2a(mcb_top)] = max_size; // initialize first MCB entry with the maximum allocation size
+  for (int i = 0x20006802; i < 0x20006C00; i += 2) { // traverse MCB entries from 0x20006802 to 0x20006BFE
+    array[m2a(i)] = 0; // initialize the current MCB to 0 to indicate unallocated memory
+    array[m2a(i + 1)] = 0; // initialize the contiguous MCB to 0 to indicate unallocated memory
   }
 }
 
@@ -231,18 +231,18 @@ void *_kalloc(int size) { return _ralloc(size, mcb_top, mcb_bot); }
  * @return the address of this deallocated space.
  */
 void *_kfree(void *ptr) {
-  //  TODO(student): add comment to each of the following line of code
-  int addr = (int)ptr;
+  // DOUBLE CHECK
+  int addr = (int)ptr; // convert pointer into int memory address
 
-  if (addr < heap_top || addr > heap_bot) {
-    return NULL;
+  if (addr < heap_top || addr > heap_bot) { // if address is outside of heap range
+    return NULL; // cannot dellocate
   }
-  int mcb_addr = mcb_top + (addr - heap_top) / 16;
+  int mcb_addr = mcb_top + (addr - heap_top) / 16; // calculate corresponding MCB entry from memory address
 
-  if (_rfree(mcb_addr) == 0) {
-    return NULL;
+  if (_rfree(mcb_addr) == 0) { // deallocate the memory at corresponding MCB entry
+    return NULL; // deallocation was unsuccessful
   }
-  return ptr;
+  return ptr; // return pointer as deallocated address
 }
 
 /*
